293e8ee0d5de6f70f3ca489cd75fb4ac641c5101
de0nnaXd <rachel.ayson@ontariotechu.net>
2024-11-23 12:20:04 -0500

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,12 @@
+/* SPA --> single page application
+client side 
+IIFE --> immediate invoked function expression
+*/
+
+(function(){
+    function Start()
+    {
+        console.log("App started");
+    }
+    window.addEventListener("load", Start);
+})();
