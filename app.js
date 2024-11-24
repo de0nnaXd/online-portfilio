1e26be1de7472ba17a55d847d5c839191992a97a
de0nnaXd <rachel.ayson@ontariotechu.net>
2024-11-24 18:19:30 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -8,27 +8,40 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+// mongo
+let mongoose = require('mongoose');
+let DB = require('./db');
+
+// mongoose to DB URI
+mongoose.connect(DB.URI);
+let mongoDB = mongoose.connection;
+mongoDB.on('error', console.error.bind(console, 'Connection Error: '));
+mongoDB.once('open', () => {
+  console.log('connected with MongoDB');
+});
+
 // to routers
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+let indexRouter = require('../routes/index');
+let usersRouter = require('../routes/users');
+let publicRouter = require('../routes/public'); 
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, '..', 'node_modules')));
 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/public', usersRouter);
+app.use('/public', publicRouter); 
 
 // shows 404 error
 app.use(function(req, res, next) {
@@ -37,17 +50,13 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error',
-    {
-      title: "Error"
-    }
-  );
+  res.render('error', {
+    title: "Error"
+  });
 });
 
 module.exports = app;
---
+++ db.js
@@ -0,0 +1,4 @@
+module.exports = {
+    URI: "mongodb+srv://rachelayson:x7IumiSlFzNoEd7w@tracker.vnmlm.mongodb.net/?retryWrites=true&w=majority&appName=Tracker"
+  };
+  
