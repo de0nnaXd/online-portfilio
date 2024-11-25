4215a90c996d95bc3289f3d5e62c574f62757453
de0nnaXd <rachel.ayson@ontariotechu.net>
2024-11-24 19:43:03 -0500

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    URI: "mongodb+srv://rachelayson:#################################/?retryWrites=true&w=majority&appName=Tracker"
+    URI: "mongodb+srv://rachelayson:x7IumiSlFzNoEd7w@tracker.vnmlm.mongodb.net/?retryWrites=true&w=majority&appName=Tracker"
   };
   
